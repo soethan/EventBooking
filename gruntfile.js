602e429623e66a27f668c4c88ddec65179bee91b
Soe Than <soethan@gmail.com>
2017-11-09 22:01:02 +0800

    add grunt to transpile scss to css (dummy css only)

---
+++ gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+			dist: {
+				files: {
+					'app/css/style.css' : 'app/css/style.scss'
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: 'app/css/*.scss',
+				tasks: ['sass']
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default',['watch']);
+}
